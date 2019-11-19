6d163dde281d5890e053eb3beb10b991fdc1a8cb
MariaFL9815 <56315627+MariaFL98@users.noreply.github.com>
2019-11-19 19:46:40 +0000

    task of the week

--- js/background.js
+++ js/background.js
@@ -9,23 +9,23 @@ $( document ).ready(function(){
 
 
   // when the button is clicked it triggers a function
-  $('h1').on('click', function () {
+  $('h1').click( function () {
 
-    // 1. log to the console that you have clicked the button
+    console.log('clicked')
 
     // this selects a randon background from above and stores it as the variable 'randomBackground'
     var randomBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)];
     // this creates a string that css will understand to be a background url
     var imageUrl = "url('./images/" + randomBackground + ".jpg')"
-    
-    
-    // 2. using jquery, change the image url of the body background to the new one saved above
 
+
+    // 2. using jquery, change the image url of the body background to the new one saved above
+$('body').css('background-image',imageUrl);
     // EXTENSION TASK!
     // 3. Change the text of the 'span' to be that of the photographer
     //   (hint, you should re-use randomBackgroun)
     // Double points if you replace '-' between their names with a space
 
-    
-  }) 
+
+  });
 }) // here endeth the function
